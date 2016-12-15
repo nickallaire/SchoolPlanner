c3b5698f97a33fe73269b0170fa64d38d64beef9
Nicholas Allaire <nallaire@ucsd.edu>
2016-12-15 12:35:41 -0800

    Initial Commit

---
+++ School Planner/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  School Planner
+//
+//  Created by Nick Allaire on 12/15/16.
+//  Copyright © 2016 Nick Allaire. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
